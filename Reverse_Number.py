1567929a68414c1c7f32257c4e401f8219628338
Aditya Kumar <adityakumar.ec06@gmail.com>
2024-12-31 13:26:44 +0530

    Update Reverse_Number.py

--- Reverse_Number.py
+++ Reverse_Number.py
@@ -1,6 +1,7 @@
+# reverse a number 
 class Reverse:
     def __init__(self,value):
-        self.value = value 
+        self.value, self.sign_bit  = (abs(value),True) if value < 0 else (value,False)
     def reverse(self):
         reverse_number = 0 
         while True:
@@ -9,4 +10,4 @@ class Reverse:
                 self.value = self.value // 10
             else:
                 break
-        return reverse_number
+        return -(reverse_number) if self.sign_bit else reverse_number
